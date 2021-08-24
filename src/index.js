a63832c091d97fd966cb3be5b6016831c525ad17
Kirill Vishenkov <vishkir1991@gmail.com>
2021-08-25 01:19:47 +0300

    feat(src/index) use routes

--- src/index.js
+++ src/index.js
@@ -1,7 +1,24 @@
 import './index.css';
 import getLoginPage from './pages/login';
+import getRegistrationPage from './pages/registration';
+import getErrorPage from './pages/error';
+
+function getCurrentPage() {
+  const { pathname } = window.location;
+
+  switch (pathname) {
+    case '/':
+      return getLoginPage();
+
+    case '/registration':
+      return getRegistrationPage();
+
+    default:
+      return getErrorPage();
+  }
+}
 
 const root = document.querySelector('body');
-const renderedTemplate = getLoginPage();
+const renderedTemplate = getCurrentPage();
 
 root.innerHTML = renderedTemplate;
