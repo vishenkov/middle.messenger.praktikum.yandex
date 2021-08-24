60df5e17b78861079c037f60b6492a95b7388fcb
Kirill Vishenkov <vishkir1991@gmail.com>
2021-08-25 02:08:01 +0300

    fix(container) use main

--- src/components/container/container.js
+++ src/components/container/container.js
@@ -2,10 +2,10 @@ import cn from '../../lib/classnames';
 import * as styles from './container.css';
 
 export default (modifiers) => `
-  <div class="${cn(styles.root, {
+  <main class="${cn(styles.root, {
     [styles.centered]: modifiers.centered,
     [styles.min100vh]: modifiers.min100vh,
   })}">
     {{children}}
-  </div>
+  </main>
 `;
