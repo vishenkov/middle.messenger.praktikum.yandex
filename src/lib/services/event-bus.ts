aa2987882d80f1b5e8458ed6bf45eb68cbee52c5
Kirill Vishenkov <vishkir1991@gmail.com>
2021-09-14 02:53:56 +0300

    feat(event-bus) use ts

--- src/lib/services/event-bus.ts
+++ src/lib/services/event-bus.ts
@@ -1,23 +1,36 @@
-class EventBus {
+type VoidFunction = (...args: unknown[]) => void;
+
+export interface IEventBus {
+  on(event: string, cb: VoidFunction): void,
+  emit(event: string, ...args: unknown[]): void,
+  off(event: string, callback: VoidFunction): void,
+}
+class EventBus implements IEventBus {
+  private listeners: Record<string, (VoidFunction)[]>;
+
   constructor() {
     this.listeners = {};
   }
 
-  on(event, callback) {
+  on(event: string, callback: VoidFunction) {
     if (!this.listeners[event]) {
       this.listeners[event] = [];
     }
     this.listeners[event].push(callback);
   }
 
-  emit(event, ...args) {
-    this.listeners[event].forEach((listener) => {
+  emit(event: string, ...args: unknown[]) {
+    this.listeners[event].forEach((listener: VoidFunction) => {
       listener(...args);
     });
   }
 
-  // Можно называть detach, как больше нравится
-  off(event, callback) {
+  off(event: string, callback: VoidFunction) {
+    if (!this.listeners[event]) {
+      return;
+    }
+
+    this.listeners[event].filter((listener: VoidFunction) => listener === callback);
   }
 }
 
