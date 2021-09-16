79b3c74d8f2dc614fe3226923b40da8dc1c98d65
Kirill Vishenkov <vishkir1991@gmail.com>
2021-09-16 23:11:58 +0300

    feat(input) support type

--- src/components/input/input.tmpl.ts
+++ src/components/input/input.tmpl.ts
@@ -2,17 +2,29 @@ import { Props } from '../../lib/base-component';
 import cn from '../../lib/classnames';
 import * as styles from './input.css';
 
-const Input = (props: Props) => `
-  <input
-    type="text"
-    class="${cn(styles, 'input', {
+const Input = (props: Props) => {
+  const valueProp = props.value
+    ? `value="${props.value}"`
+    : '';
+
+  const typeProp = props.type ?? 'text';
+
+  const classNames = cn(styles, 'input', {
     'input_full-width': props.fullWidth,
     'input_gutter-bottom': props.gutterBottom,
     input_error: props.error,
-  })}"
-    placeholder="${props.placeholder}"
-    name="${props.name}"
-  />
-`;
+  });
+
+  return `
+    <input
+      type="${typeProp}"
+      class="${classNames}"
+      placeholder="${props.placeholder}"
+      name="${props.name}"
+      onBlur={{handleBlur}}
+      ${valueProp}
+    />
+  `;
+};
 
 export default Input;
--- src/components/input/input.ts
+++ src/components/input/input.ts
@@ -2,11 +2,43 @@ import BaseComponent, { Props } from '../../lib/base-component';
 import getInputTmpl from './input.tmpl';
 
 import Native from '../native';
+import isEqual from '../../lib/utils/is-equal';
+import FormValidator from '../../lib/services/form-validator';
 
+const initialState = {
+  error: false,
+  value: '',
+};
 class Input extends BaseComponent {
+  formValidator: FormValidator;
+
   constructor(props: Props) {
-    super(props, {
+    super({ ...initialState, ...props }, {
       Native,
+    });
+
+    this.formValidator = new FormValidator();
+  }
+
+  componentDidUpdate(oldProps: Props, newProps: Props) {
+    return !isEqual(oldProps, newProps);
+  }
+
+  handleBlur(e) {
+    const { value } = e.currentTarget;
+    const isValid = this.props.validate
+      ? this.formValidator.prop(this.props.validate).validate(value)
+      : true;
+
+    this.setProps({
+      error: !isValid,
+      value,
+    });
+  }
+
+  registerHandlers() {
+    this.setHandlers({
+      handleBlur: this.handleBlur.bind(this),
     });
   }
 
