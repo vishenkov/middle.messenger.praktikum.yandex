51ac0ad8c788b2c74c4aef4dd1ace445df55626d
Kirill Vishenkov <vishkir1991@gmail.com>
2021-09-16 23:11:30 +0300

    feat(button) support type

--- src/components/button/button.tmpl.ts
+++ src/components/button/button.tmpl.ts
@@ -2,15 +2,24 @@ import { Props } from '../../lib/base-component';
 import cn from '../../lib/classnames';
 import * as styles from './button.css';
 
-const Button = (props: Props) => `
-  <button class="${cn(styles, 'button', {
+const Button = (props: Props) => {
+  const typeProp = props.type ?? 'button';
+
+  const classNames = cn(styles, 'button', {
     'button_full-width': props.fullWidth,
     'button_gutter-bottom': props.gutterBottom,
     button_primary: !props.secondary,
     button_secondary: props.secondary,
-  })}">
-    <span class="${styles.button__text}">${props.label}</span>
-  </button>
+  });
+
+  return `
+    <button
+      class="${classNames}"
+      type="${typeProp}"
+    >
+      <span class="${styles.button__text}">${props.label}</span>
+    </button>
 `;
+};
 
 export default Button;
