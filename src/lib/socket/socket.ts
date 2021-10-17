f4bfad4fce5b0966f1a97e69842f79d4337c64f1
Kirill Vishenkov <vishkir1991@gmail.com>
2021-10-17 22:58:58 +0300

    feat(socket) socket lib

---
+++ src/lib/socket/socket.ts
@@ -0,0 +1,47 @@
+import EventBus from '../services/event-bus';
+import { EVENTS } from './types';
+
+class Socket extends EventBus {
+  _baseApiUrl = 'wss://ya-praktikum.tech/ws/chats';
+
+  socket: WebSocket;
+
+  _url: string;
+
+  constructor(url: string) {
+    super();
+    this._url = url;
+  }
+
+  start() {
+    const socket = new WebSocket(`${this._baseApiUrl}/${this._url}`);
+    this.socket = socket;
+
+    socket.addEventListener('open', () => {
+      console.log('Соединение установлено');
+      this.emit(EVENTS.open);
+    });
+
+    socket.addEventListener('close', (event) => {
+      this.emit(EVENTS.close, event);
+    });
+
+    socket.addEventListener('message', (event) => {
+      this.emit(EVENTS.message, event.data);
+    });
+
+    socket.addEventListener('error', (event) => {
+      this.emit(EVENTS.error, event);
+    });
+  }
+
+  sendMessage(data: Record<string, unknown>) {
+    return this.socket.send(JSON.stringify(data));
+  }
+
+  destroy() {
+    this.socket.close();
+  }
+}
+
+export default Socket;
